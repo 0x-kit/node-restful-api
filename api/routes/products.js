1786adc4b7f4a58f969dfa2842d1817c46055fa1
0xkush <camartinez@esei.uvigo.es>
2018-04-20 15:54:43 +0200

    Adds User Login & JWT Signing

--- api/routes/products.js
+++ api/routes/products.js
@@ -1,8 +1,8 @@
 const express = require('express');
 const router = express.Router();
+const mongoose = require('mongoose');
 
 const Product = require('../models/product');
-const mongoose = require('mongoose');
 
 const multer = require('multer');
 const storage = multer.diskStorage({
